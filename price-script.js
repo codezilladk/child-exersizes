a0049f5a0a25a8b2c0fcb762806323599a440716
codezilladk <46768429+codezilladk@users.noreply.github.com>
2020-06-19 15:54:16 +0300

    Update price-script.js

--- price-script.js
+++ price-script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.js-product-price').each(function(index, value) {
-	  console.log($(this).text());
+    	var finalPrice = (parseInt($(this).text()) + (parseInt($(this).text())*0.2))*2;
+	  console.log(finalPrice);
 	});
 });
