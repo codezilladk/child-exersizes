6bab868c078661ba46f8aa9fcec139d601f984e5
codezilladk <46768429+codezilladk@users.noreply.github.com>
2020-06-19 16:07:02 +0300

    Update price-script.js

--- price-script.js
+++ price-script.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
+	let curs = 78;
+	$.get( "https://www.cbr-xml-daily.ru/daily_json.js")
+	  .done(function( data ) {
+	    curs = data;
+	  });
     $('.js-product-price').each(function(index, value) {
-    	var finalPrice = (parseInt($(this).text()) + (parseInt($(this).text())*0.2))*2;
+    	if(curs === 78) {
+    		finalPrice = (parseInt($(this).text()) + (parseInt($(this).text())*0.2))*2*78;
+    	} esle {
+    		finalPrice = (parseInt($(this).text()) + (parseInt($(this).text())*0.2))*2*curs.Valute.EUR.value;
+    	}
+    	
 	  console.log(finalPrice);
 	});
 });
